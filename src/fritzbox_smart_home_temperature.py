970ef63cf1c5d761cf292d944246c899dc4e81f5
Christoph Massmann <cm@vianetz.com>
2023-12-13 14:16:42 +0100

    added unit tests for smart home plugin
    Signed-off-by: Christoph Massmann <cm@vianetz.com>

--- src/fritzbox_smart_home_temperature.py
+++ src/fritzbox_smart_home_temperature.py
@@ -5,13 +5,17 @@
   @see https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/x_homeauto.pdf
 """
 
-import sys
 from fritzconnection import FritzConnection
 from fritzbox_config import FritzboxConfig
 from fritzbox_munin_plugin_interface import MuninPluginInterface,main_handler
 
 
 class FritzboxSmartHomeTemperature(MuninPluginInterface):
+  __connection = None
+
+  def __init__(self, fritzbox_connection: FritzConnection):
+    self.__connection = fritzbox_connection
+
   def print_stats(self):
     """get the current cpu temperature"""
 
@@ -32,24 +36,15 @@ class FritzboxSmartHomeTemperature(MuninPluginInterface):
 
   def __retrieve_smart_home_temps(self):
     smart_home_data = []
-    config = FritzboxConfig()
-
-    try:
-      connection = FritzConnection(address=config.server, user=config.user, password=config.password, use_tls=config.use_tls)
-    except Exception as e:
-      sys.exit("Couldn't get temperature: " + str(e))
 
     for i in range(0, 20):
-      try:
-        data = connection.call_action('X_AVM-DE_Homeauto1', 'GetGenericDeviceInfos', arguments={'NewIndex': i})
-        if (data['NewTemperatureIsEnabled']):
-          smart_home_data.append(data)
-      except Exception as e:
-        # smart home device index does not exist, so we stop here
-        break
+      data = self.__connection.call_action('X_AVM-DE_Homeauto1', 'GetGenericDeviceInfos', arguments={'NewIndex': i})
+      if 'NewTemperatureIsEnabled' in data and data['NewTemperatureIsEnabled']:
+        smart_home_data.append(data)
 
     return smart_home_data
 
 
 if __name__ == '__main__':
-  main_handler(FritzboxSmartHomeTemperature())
+  config = FritzboxConfig()
+  main_handler(FritzboxSmartHomeTemperature(FritzConnection(address=config.server, user=config.user, password=config.password, use_tls=config.use_tls)))
